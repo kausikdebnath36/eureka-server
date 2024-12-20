d340004c7402d130874a747884ff23ca7c022a0e
kausik debnath <kausikdebna1920@gmail.com>
2024-12-20 15:47:09 +0530

    init

---
+++ src/main/java/com/debnath/Eureka_Server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.debnath.Eureka_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
